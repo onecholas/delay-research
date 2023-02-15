997e5a3565ecd60dce5b0c7e513134ca52d23f4d
onecholas <nickhsu2025@u.northwestern.edu>
2023-02-15 10:00:35 -0600

    Created initial files

---
+++ delay_calc.py
@@ -0,0 +1,13 @@
+# import 
+
+
+def delay(design, input_pairs):
+    pass
+    
+
+# Example Script:
+# module bitwiseor(input[7:0] num1, 
+#              input[7:0] num2, 
+#              output[7:0] out);
+#       assign out[7:0] = num1[7:0] | num2[7:0]
+# endmodule
