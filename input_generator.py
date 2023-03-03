c847654946130f60e8451f07e8494ae357b7ad14
onecholas <nickhsu2025@u.northwestern.edu>
2023-03-03 14:34:41 -0600

    Added sparse input generator based on probability

--- input_generator.py
+++ input_generator.py
@@ -18,6 +18,20 @@ def sparse_input_generator(verilog_module, input_pairs_file):
                         writer.writerow(default)
                         default = [0 for idx in range(len(inputs))]
 
+def sparse_input_generator_probability(verilog_module, input_pairs_file, prob: float):
+        inputs = parse_verilog(verilog_module).inputs
+        inputs = inputs + inputs
+        default = [0 for idx in range(len(inputs))]
+
+        with open(input_pairs_file, 'w', newline='', encoding='utf-8-sig') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(inputs)
+                for idx in range(len(inputs)):
+                        for idx in range(len(inputs)):
+                                # What the random input should be
+                                default[idx] = random.choices([0,1], weights=(prob, 1 - prob), k = 1)[0]
+                        writer.writerow(default)
+
 def random_input_generator(verilog_module, input_pairs_file):
 
         inputs = parse_verilog(verilog_module).inputs
@@ -32,5 +46,6 @@ def random_input_generator(verilog_module, input_pairs_file):
                         writer.writerow(row)
 
 if __name__ == "__main__":
-        # sparse_input_generator("test2/adder.v", "test2/adder_rand_inputs.csv")
-        random_input_generator("test2/adder.v", "test2/adder_rand_inputs.csv")
+        # sparse_input_generator("test2/adder_module.v", "test2/adder_rand_inputs.csv")
+        # random_input_generator("test2/adder_module.v", "test2/adder_rand_inputs.csv")
+        sparse_input_generator_probability("test2/adder_module.v", "test2/adder_rand_inputs.csv", 0.5)
